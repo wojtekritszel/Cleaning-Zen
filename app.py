d801994cfbcd485458e6d55eac3354ceee2c9174
Wojtello <wojtekritszel@wp.pl>
2020-12-11 00:54:58 +0100

    Dodano tlo dla wszystkich widokow  / dodano wybor stanu z listy oraz mozliwosc dodania zdjecia

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, DateField, IntegerField
+from wtforms import StringField, SubmitField, DateField, SelectField, FileField
 from wtforms.validators import DataRequired
 import os
 
@@ -34,7 +34,11 @@ class ItemName(FlaskForm):
     name = StringField('Nazwa przedmiotu', validators=[DataRequired()])
     description = StringField('Opis przedmiotu')
     purchase_date = DateField('Data zakupu')
-    condition = IntegerField('Stan w skali 1-10')
+    condition = SelectField('Stan w skali 1-10', choices=[('tragiczny - 1'), ('mocno zniszczony - 2'),
+                                                          ('zniszczony - 3'), ('znoszony - 4'), ('ślady użytkowania - 5'),
+                                                          ('przeciętny - 6'), ('dobry - 7'), ('bardzo dobry - 8'),
+                                                          ('idealny - 9'), ('nowy - 10')])
+    image = FileField('Zdjęcie przedmiotu')
     submit = SubmitField('Wyślij')
 
 
